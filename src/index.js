67c18b5e94ab62661bafbd7c436cfcb0678497e7
santosh <santosh.ece06@gmail.com>
2018-07-12 16:04:06 +0530

    react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Button} from 'reactstrap';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 
 let firstName = "Kharghar";
 const myelement = (<h1>ReactApp Element @{firstName}</h1>);
@@ -28,3 +30,11 @@ ReactDOM.render(elem1, document.getElementById('root'));
 ReactDOM.render(myelement, document.getElementById('root1'));
 
 setInterval(dynamicContent, 1000);
+
+const BootElem = (props) => {
+    return (
+      <Button color="danger">Danger!</Button>
+    );
+};
+const b1 = <Button color="danger">Danger!</Button>;
+ReactDOM.render(<BootElem />, document.getElementById('boot1'));
